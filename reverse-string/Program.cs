8e502b4d118fed88ac0fb68e8ffc776ae70c2481
sharaddhole-dotnet-dev <sharaddhole2014@gmail.com>
2026-04-01 18:30:08 +0530

    Add reverse-string, palindrome-string programs

---
+++ reverse-string/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string input = "madam";
+        char[] arr = input.ToCharArray();
+        Array.Reverse(arr);
+        string result = new string(arr);
+
+        Console.WriteLine(result);
+    }
+}
